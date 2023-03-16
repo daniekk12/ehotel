722e65320de9510c5162e05394590f845f3e913a
daniekk12 <alexandrudanielaka47@gmail.com>
2023-03-16 14:03:18 +0200

    salut sunt sexy

--- CodeCool.EhotelBuffet/Refill/Service/RefillService.cs
+++ CodeCool.EhotelBuffet/Refill/Service/RefillService.cs
@@ -6,7 +6,16 @@ public class RefillService : IRefillService
 {
     public IEnumerable<Portion> AskForRefill(Dictionary<MenuItem, int> menuItemToPortions)
     {
-        throw new NotImplementedException();
+        List<Portion> portions = new List<Portion>();
+        foreach (var itemToPortion in menuItemToPortions)
+        {
+            for (int i = 0; i < itemToPortion.Value; i++)
+            {
+                portions.Add(new Portion(itemToPortion.Key,DateTime.Now));
+            }
+        }
+
+        return portions;
     }
 }
 
